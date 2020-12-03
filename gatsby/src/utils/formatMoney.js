ae88a61122a0592b20789bf324c1e28704b64b5c
Will Foster <forest_5592@hotmail.co.uk>
2020-12-03 19:00:05 +0000

    first commit

--- gatsby/src/utils/formatMoney.js
+++
@@ -1,8 +0,0 @@
-const formatter = Intl.NumberFormat('en-gb', {
-  style: 'currency',
-  currency: 'GBP',
-});
-
-export default function formatMoney(cents) {
-  return formatter.format(cents / 100);
-}
